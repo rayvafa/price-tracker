f88251d339eb9e0887019a191e6d8a8390849ac7
Ray Vafa <vafatabar@gmail.com>
2017-04-04 22:02:27 +1000

    Basic setup for serverless framework

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.priceWatcher = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -10,7 +10,4 @@ module.exports.hello = (event, context, callback) => {
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
